efba73a89aaa408d1d2119bff97ec2d07c1fb1f8
ashwinii <ashwinipowar2111@gmail.com>
2026-04-03 23:39:59 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,30 @@
+# set doesnt take the repeated valuesonly once it printed
+# sets are unorderable collection of data . they storfe multiple items in a singke varianle . set item are seprated by the commas and enclosed within the
+# curly brackets {}.
+# sets are unchangable means you cant change the items of the sets you created 
+# sets do nit contain duplicate items
+
+
+
+s={2,4,2,5}
+print(s) 
+# here we got the output of the 24 nad 5 nit repeat the value of the 2
+
+
+info={"carla",19, False,5.8, 19 }
+print(info)
+# no garenty to maintain the order
+
+
+harry={}
+print(type(harry))
+# when we write like this it become the empty dict not a empty set for the empty set we write as
+
+
+harry=set()
+print(type(harry))
+# this is the empty set
+
+for value in info:
+    print(value)
+# set oeder maintain nahi karta
