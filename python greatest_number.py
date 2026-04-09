ab0f762e395712d53503d5863a87ced1da17abfe
ashwinii <ashwinipowar2111@gmail.com>
2026-04-09 22:25:53 +0530

    Add files via upload

---
+++ python greatest_number.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+
+# Find the greatest number
+greatest = max(num1, num2, num3, num4)
+
+# Display the result
+
+
+print("The greatest number is:", greatest)
